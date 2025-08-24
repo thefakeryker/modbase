407a47463446ea055720b942aeabee56fd84c116
avn <thefakeryker@gmail.com>
2025-08-23 22:43:53 -0700

    Backup

---
+++ cmake/identity.cmake
@@ -0,0 +1,12 @@
+set(PROJECT_NAME ioq3)
+set(PROJECT_VERSION 1.36)
+
+set(BASEGAME baseq3)
+
+set(CGAME_MODULE cgame)
+set(GAME_MODULE qagame)
+set(UI_MODULE ui)
+
+set(COPYRIGHT "QUAKE III ARENA Copyright © 1999-2000 id Software, Inc. All rights reserved.")
+
+set(PROTOCOL_HANDLER_SCHEME quake3)
