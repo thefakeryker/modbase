08c3381ce37ab80ad8eeb369f5179cbe2cd56207
avn <thefakeryker@gmail.com>
2025-08-23 23:58:11 -0700

    Used unifdef to get rid of MISSIONPACK

--- code/cgame/cg_newdraw.c
+++ code/cgame/cg_newdraw.c
@@ -20,9 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#ifndef MISSIONPACK
 #error This file not be used for classic Q3A.
-#endif
 
 #include "cg_local.h"
 #include "../ui/ui_shared.h"
--- code/ui/ui_shared.c
+++ code/ui/ui_shared.c
@@ -3290,7 +3290,6 @@ static bind_t g_bindings[] =
 
 static const int g_bindCount = ARRAY_LEN(g_bindings);
 
-#ifndef MISSIONPACK
 static configcvar_t g_configcvars[] =
 {
 	{"cl_run",			0,					0},
@@ -3303,7 +3302,6 @@ static configcvar_t g_configcvars[] =
 	{"cl_freelook",		0,					0},
 	{NULL,				0,					0}
 };
-#endif
 
 /*
 =================
@@ -5924,10 +5922,8 @@ displayContextDef_t *Display_GetContext(void) {
 	return DC;
 }
  
-#ifndef MISSIONPACK
 static float captureX;
 static float captureY;
-#endif
 
 void *Display_CaptureItem(int x, int y) {
 	int i;
